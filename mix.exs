66a5b915fe097ca475c195074a724fe3469788a8
John Davenport <johns10@gmail.com>
2022-06-10 17:28:16 +0800

    package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule Pexelmatch.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @url "https://github.com/user-docs/pexelmatch"
+  @description """
+  Pure Elixir pixel-level image comparison library. A rewrite of the excellent pixelmatch library.
+  """
+
   def project do
     [
       app: :pexelmatch,
-      version: "0.0.1",
+      description: @description,
+      version: @version,
+      url: @url,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -24,6 +33,14 @@ defmodule Pexelmatch.MixProject do
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:arrays, "~> 2.0"},
       {:ex_png, "~> 1.0.0"}
+    ]
+  end
+
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @url}
     ]
   end
 end
